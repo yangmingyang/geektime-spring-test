4ddd3f19ecd5cf6b17c157c57b0819f21539ba75
yangmy <1505570288@qq.com>
2019-07-19 09:51:13 +0800

    lambda表达式

--- javaTest/src/com/mingyang/lambda/main/testInterface01.java
+++ javaTest/src/com/mingyang/lambda/main/testInterface01.java
@@ -1,7 +1,17 @@
 package com.mingyang.lambda.main;
 
+import com.mingyang.lambda.Interfaces.Impl.Father;
+
 public class testInterface01 {
     public static void main(String[] args) {
-        
+        Father fa = new Father(){
+            @Override
+            public void speak() {
+                super.speak();
+                System.out.printf("my会说");
+            }
+        };
+        System.out.printf(fa.toString());
+       // fa.speak();
     }
 }
