95f13a3bb55f41ede727ab9b534686283b614fd1
yangmy <1505570288@qq.com>
2019-07-29 08:21:32 +0800

    File判断获取方法

---
+++ javaTest/src/com/mingyang/File/Demo05File.java
@@ -0,0 +1,21 @@
+package com.mingyang.File;
+
+import org.junit.Test;
+
+/**
+ *  File类创建删除功能的方法
+ *       boolean createNewFile() 当且仅当具有该名称的文尚不存在时，创建一个新的空文件。
+ *       boolean delete() 删除此抽象路径名表示的文件或目录。
+ *       boolean mkdir() 创建此抽象路径名指定的目录。
+ *       boolean mkdirs() 创建此抽象路径名指定的目录，包括所有必需但不存在的父目录。
+ */
+public class Demo05File {
+    @Test
+    public void demo01(){
+        /**
+         * boolean createNewFile() 当且仅当具有该名称的文尚不存在时，创建一个新的空文件。
+         */
+
+
+    }
+}
