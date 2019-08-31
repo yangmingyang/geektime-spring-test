5f307653b1a3bedd5dfeda5a435f5054de07cd08
yangmy <1505570288@qq.com>
2019-08-31 23:11:47 +0800

    jedis

---
+++ javaTest/src/com/redis/jedis/JedisPoolUtils.java
@@ -0,0 +1,33 @@
+package com.redis.jedis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class JedisPoolUtils {
+    private static JedisPool jedisPool;
+    static {
+        //读取配置文件
+        InputStream is = JedisPoolUtils.class.getClassLoader().getResourceAsStream("jedis.properties");
+        //创建Properties对象
+        Properties pro = new Properties();
+        try {
+            pro.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(Integer.parseInt(pro.getProperty("maxTotal")));
+        config.setMaxIdle(Integer.parseInt(pro.getProperty("maxIdle")));
+        jedisPool = new JedisPool(config,pro.getProperty("host"),Integer.parseInt(pro.getProperty("port")));
+    }
+
+    public static Jedis getJedis(){
+        return jedisPool.getResource();
+    }
+}
+
